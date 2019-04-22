32c07790e6aded98b7635e8f9708d903ea405a2e
ulil albab <ulilalbabtiuii@gmail.com>
2019-04-22 14:53:28 +0200

    First Day
    Add material from Kaggle

--- day1.py
+++ day1.py
@@ -59,3 +59,30 @@ if isinstance(myfloat, float) and myfloat == 10.0:
 if isinstance(myint, int) and myint == 20:
     print("Integer: %d" % myint)
 
+# Kaggle
+print(5//2)
+print(6//2)
+
+# min & max
+print(min(1,2,3))
+print(max(1,2,3))
+
+ages = [93 , 99, 66 ,17, 85 , 89, 77]
+youngest = min(ages)
+print(youngest)
+
+# Floar
+print(float(10))
+print(int(3.33))
+# even be called on strings!
+print(int('807') + 1)
+
+
+#Exercise: Syntax, Variables, & Numbers
+print("Congratulations!")
+
+pi = 3.14159
+diameter = 3
+radius = 0.5 * diameter
+area = pi * radius^2
+print(area)
