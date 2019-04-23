d4c80dd3e6f096468f64605d12c4b9dc70379c2e
ulil albab <ulilalbabtiuii@gmail.com>
2019-04-23 21:11:24 +0200

    Dynamic reference

--- day2.py
+++ day2.py
@@ -4,9 +4,7 @@ nested_tup = (4, 5, 6), (7, 8)
 nested_tup
 print(nested_tup)
 
-a = 5
-a
-print(a)
+
 
 
 
@@ -18,3 +16,33 @@ print(data)
 from numpy.random import randn
 data1 = {i : randn() for i in range(7)}
 print(data1)
+
+# Append
+a = [1,2,3]
+b = a
+a.append(4)
+b
+print(b)
+
+# Append Element
+def append_element(some_list, element):
+    some_list.append(element)
+data = [1, 2, 3]
+append_element(data, 4)
+data
+print(data)
+
+# Dynamic references
+d = 5
+type(d)
+print(type(d)) # can identify class (int)
+
+e = "Mantul Dab" # can identify as string
+type(e)
+print(type(e))
+
+
+
+
+
+
