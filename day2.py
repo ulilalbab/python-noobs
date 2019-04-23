3450bfa3d49404b76736c1b740aa4e50b8858779
ulil albab <ulilalbabtiuii@gmail.com>
2019-04-23 21:38:49 +0200

    Attributes

--- day2.py
+++ day2.py
@@ -41,7 +41,25 @@ e = "Mantul Dab" # can identify as string
 type(e)
 print(type(e))
 
+f = 4.5
+g = 2
 
+f / g
+print(f/g)
+
+a = 5
+isinstance(a, int)
+print(isinstance(a,int))
+
+a = 5; b = 4.5
+isinstance(a, (int, float))
+print(isinstance(a, (int, float)))
+
+isinstance(b, (int, float))
+print(isinstance(b, (int, float)))
+
+# Attributes and Methods
+# Page 35
 
 
 
