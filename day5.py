0bdc3f03b62e8554905925fe2c679fe3ecf71610
ulil albab <ulilalbabtiuii@gmail.com>
2019-05-01 17:42:30 +0200

    day 5 date

---
+++ day5.py
@@ -0,0 +1,32 @@
+a = 'one way of writing a string'
+b = "another way"
+
+val = "espanol"
+val
+print(val)
+#type casting
+s = '3.14159'
+fval = float(s)
+type(fval)
+print(type(fval))
+
+int(fval)
+print(int(fval))
+
+bool(fval)
+print(bool(fval))
+
+bool(0)
+print(bool(0))
+
+#none
+a = None
+a is None
+print(a is None)
+
+b = 5
+b is not None
+
+
+#page 46
+
